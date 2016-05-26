e54ed3037e6ac181965f8c102cffb00fbbe1cf8b
himanshup <himanshup@winjit.com>
2016-05-26 12:50:31 +0530

    some

--- app/src/main/java/com/himanshu/frameworkdemo/registration/RegistrationActivity.java
+++ app/src/main/java/com/himanshu/frameworkdemo/registration/RegistrationActivity.java
@@ -3,14 +3,12 @@ package com.himanshu.frameworkdemo.registration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.himanshu.frameworkdemo.R;
-
 public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+//        setContentView(R.layout.activity_registration);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 //
